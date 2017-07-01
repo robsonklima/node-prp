e46de5efb49ce133f5e4047a30d1290952c7e1d5
Robson Lima <robsonklima@gmail.com>
2017-07-01 20:30:39 -0300

    Add put on project api

--- routes/projects.js
+++ routes/projects.js
@@ -34,7 +34,7 @@ ROUTER.prototype.handleRoutes = function(router, pool) {
                 if(err) {
                     res.status(400).send({"error": true, "details": err});
                 } else {
-                    res.status(200).send({"error": false,  project});
+                    res.status(200).send({"error": false, project});
                 }
             });
         });
@@ -58,6 +58,29 @@ ROUTER.prototype.handleRoutes = function(router, pool) {
                     res.status(400).send({"error": true, "details": err});
                 } else {
                     res.status(200).send({"error": false, details, "project": req.body});
+                }
+            });
+        });
+    });
+
+    router.put("/projects/:id", function(req, res) {
+        var query = "UPDATE ?? SET ?? = ?, ?? = ? WHERE ?? = ?";
+        var vars = ["projects"
+          , "name", req.body.name
+          , "scope", req.body.scope
+
+          , "id", req.params.id
+        ];
+        query = mysql.format(query, vars);
+        pool.getConnection(function(err, connection) {
+            connection.query(query, function(err, details) {
+                connection.release();
+                if(err) {
+                    res.status(400).send({"error": true, "details": err});
+                } else if (details.affectedRows == 0) {
+                    res.status(400).send({"error": false, details });
+                } else {
+                    res.status(200).send({"error": false, details, "user": req.body });
                 }
             });
         });
